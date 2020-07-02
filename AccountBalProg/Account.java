eccd081e7cd8f25cdbdedadf734f3cced7b9c264
Courtnee Sullivan <52513651+PresidentCourtnee@users.noreply.github.com>
2020-07-01 18:54:55 -0600

    This is my reworked Wk 1 Assignment for PRG321

--- AccountBalProg/Account.java
+++ AccountBalProg/Account.java
@@ -1,25 +1,34 @@
-
 /**
- * Write a description of class Account here. Week 1 Assignment ?1 Answer
- *for PRG321 - Jerrie Pineda
- * @author (Courtnee)
+ * Project: Week 1 Assignment Answer to Question #1
+ * Class PRG321
+ * Instructor: Jerrie Pineda
+  * @author (Courtnee)
  * @version (6-26-2020)
+ * This is my Account class and is the requested main class for the assignment
+ * and provides the behaviors
  */
 public class Account
 {
     private double balance = 0.0;
-    
-    //construc
-    public Account(double PrimBal)
+    //this is the constructor part of this class
+        public Account(double InitBal)
     {
-        balance = PrimBal;
+        balance = InitBal;
     }
-    public double getNewBal()
+    // this is the Accessment point of the class
+    public double getBalance()
     { 
         return balance;
     }
+    // This is the way to manage the account interest
     public void addInt(double interest)
     {
-        balance = balance * interest;
+        balance += balance * interest;
+    }
+    //This is me overriding the toString
+    @Override
+    public String toString()
+    {
+        return "The balance is: " + balance;
     }
 }
--- AccountBalProg/AccountTest.java
+++ AccountBalProg/AccountTest.java
@@ -1,40 +1,35 @@
-import static org.junit.Assert.*;
+import org.junit.Assert.*;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 /**
- * Write a description of class Account here. Week 1 Assignment ?1 Answer
- *for PRG321 - Jerrie Pineda
- * @author (Courtnee)
+ * Project: Week 1 Assignment Answer to Question #1
+ * Class PRG321
+ * Instructor: Jerrie Pineda
+  * @author (Courtnee)
  * @version (6-26-2020)
+ * This is the test class for Account
  */
 public class AccountTest
-{
-    private Account account;
-    /**
-     * This is called before every test case method.
-     */
-    @Before
-    public void setUp()
-    {
-        account = new Account(1000);
-    }
+{// this is a set up class
+    private Account account = new Account(1000);
+    
+    // this is going to test 
     @Test
-    public void testaddInt()
+    public void testInterest()
     {
-        double newBalance = account.addInt(.05); 
+        account.addInt(.05);
+        System.out.println("Interest after 1st year: "+account.getBalance());
+        assert(account.getBalance()==1050);
         
         account.addInt(.05);
-        System.out.print("Interest after 1st year: " + account.getBalance());
-        assert(newBalance == 1050);
+        System.out.println("Interest after 2nd year: "+account.getBalance());
+        assert(account.getBalance()==1102.5);
         
         account.addInt(.05);
-        System.out.print("Interest after 2nd year: " + account.getBalance());
-        assert(newBalance() == 1102.5);
-        
-        account.addInt(.05);
-        System.out.print("Interest after 3rd year: "+ account.getBalanace());
-        assert(newBalance() ==1157.625);
+        System.out.println("Interest after 3rd year: "+account.getBalance());
+        assert(account.getBalance()==1157.625);
+               
     }
 }
